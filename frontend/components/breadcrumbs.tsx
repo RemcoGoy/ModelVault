9d07e2866b3f0fe1628ceffff9ea8c50a9f891cd
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2024-04-23 17:06:11 +0200

    Dynamic breadcrumbs

--- frontend/components/breadcrumbs.tsx
+++ frontend/components/breadcrumbs.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -7,27 +9,39 @@ import {
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
 import Link from "next/link"
+import { usePathname } from "next/navigation";
+import humanizeString from "humanize-string"
 
 export function Breadcrumbs() {
+    const pathname = usePathname();
+
     return (
         // TODO: Make dynamic
         <Breadcrumb className="hidden md:flex">
             <BreadcrumbList>
-                <BreadcrumbItem>
-                    <BreadcrumbLink asChild>
-                        <Link href="#">Dashboard</Link>
-                    </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                    <BreadcrumbLink asChild>
-                        <Link href="#">Orders</Link>
-                    </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                    <BreadcrumbPage>Recent Orders</BreadcrumbPage>
-                </BreadcrumbItem>
+                {pathname.split("/").filter(k => k !== "").map((pathPart, index) => {
+                    const splits = pathname.split("/").filter(k => k !== "")
+                    const noSplits = splits.length
+
+                    if (noSplits === index + 1) {
+                        return (
+                            <BreadcrumbItem key={pathPart}>
+                                <BreadcrumbPage>{humanizeString(pathPart)}</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        )
+                    } else {
+                        return (
+                            <>
+                                <BreadcrumbItem key={pathPart}>
+                                    <BreadcrumbLink asChild>
+                                        <Link href={splits.slice(0, index + 1).join("/")}>{humanizeString(pathPart)}</Link>
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator />
+                            </>
+                        )
+                    }
+                })}
             </BreadcrumbList>
         </Breadcrumb>
     )
