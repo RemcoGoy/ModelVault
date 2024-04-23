c551edab42da4c954f97555d3269c3a616e5b5ec
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2024-04-23 15:42:53 +0200

    Working on layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,12 @@ import { cn } from "@/lib/utils"
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/sonner"
+import { Sidebar } from "@/components/sidebar";
+import { Providers } from "@/components/providers";
+import { Breadcrumbs } from "@/components/breadcrumbs";
+import { SheetMenu } from "@/components/sheetmenu";
+import { SearchComponent } from "@/components/search";
+import { ProfileMenu } from "@/components/profilemenu";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -18,10 +24,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head />
       <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable, inter.className)}>
-        {children}
-        <Toaster />
+        <Toaster position="top-center" closeButton />
+        <Providers
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex min-h-screen w-full flex-col bg-muted/40">
+            <Sidebar />
+            <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+              <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+                <SheetMenu />
+                <Breadcrumbs />
+                <SearchComponent />
+                <ProfileMenu />
+              </div>
+            </header>
+            {children}
+          </div>
+        </Providers>
       </body>
     </html>
   );
---
+++ frontend/components/profilemenu.tsx
@@ -0,0 +1,36 @@
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+import { User } from 'lucide-react';
+
+
+export function ProfileMenu() {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    variant="outline"
+                    size="icon"
+                    className="overflow-hidden rounded-full"
+                >
+                    <User />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
