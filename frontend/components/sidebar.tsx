4e847d1a1283bcbc0865df551914a393e2a09090
RemcoGoy <remco.goy@hotmail.com>
2024-04-24 21:09:45 +0200

    Dynamic sidebar items

--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -1,12 +1,10 @@
+'use client'
+
 import Link from "next/link";
 import {
     Home,
-    LineChart,
-    Package,
     Package2,
     Settings,
-    ShoppingCart,
-    Users2,
 } from "lucide-react"
 
 import {
@@ -14,85 +12,54 @@ import {
     TooltipContent,
     TooltipTrigger,
 } from "@/components/ui/tooltip"
+import { usePathname } from "next/navigation";
+
+const sidebarItems = [
+    {
+        "title": "Dashboard",
+        "path": "/dashboard",
+        "icon": <Home className="h-5 w-5" />
+    }
+]
 
 export function Sidebar() {
+    const pathname = usePathname();
+
     return (
         <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
             <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
                 <Link
-                    href="#"
+                    href="/dashboard"
                     className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
                 >
                     <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-                    <span className="sr-only">Acme Inc</span>
+                    <span className="sr-only">Model Vault</span>
                 </Link>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <Link
-                            href="#"
-                            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                        >
-                            <Home className="h-5 w-5" />
-                            <span className="sr-only">Dashboard</span>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Dashboard</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <Link
-                            href="#"
-                            className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                        >
-                            <ShoppingCart className="h-5 w-5" />
-                            <span className="sr-only">Orders</span>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Orders</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <Link
-                            href="#"
-                            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                        >
-                            <Package className="h-5 w-5" />
-                            <span className="sr-only">Products</span>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Products</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <Link
-                            href="#"
-                            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                        >
-                            <Users2 className="h-5 w-5" />
-                            <span className="sr-only">Customers</span>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Customers</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <Link
-                            href="#"
-                            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-                        >
-                            <LineChart className="h-5 w-5" />
-                            <span className="sr-only">Analytics</span>
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent side="right">Analytics</TooltipContent>
-                </Tooltip>
+                {
+                    sidebarItems.map(item => {
+                        return (
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Link
+                                        href={item.path}
+                                        className={`flex h-9 w-9 items-center justify-center rounded-lg transition-colors hover:text-foreground md:h-8 md:w-8 ${item.path === pathname ? "bg-accent text-accent-foreground" : "text-muted-foreground"}`}
+                                    >
+                                        {item.icon}
+                                        <span className="sr-only">{item.title}</span>
+                                    </Link>
+                                </TooltipTrigger>
+                                <TooltipContent side="right">{item.title}</TooltipContent>
+                            </Tooltip>
+                        )
+                    })
+                }
             </nav>
             <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
                 <Tooltip>
                     <TooltipTrigger asChild>
                         <Link
                             href="/dashboard/settings"
-                            className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                            className={`flex h-9 w-9 items-center justify-center rounded-lg transition-colors hover:text-foreground md:h-8 md:w-8 ${pathname === "/dashboard/settings" ? "bg-accent text-accent-foreground" : "text-muted-foreground "}`}
                         >
                             <Settings className="h-5 w-5" />
                             <span className="sr-only">Settings</span>
