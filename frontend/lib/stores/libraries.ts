2b5750da993bb987a62ab85caf940deb87498eb3
RemcoGoy <remco.goy@hotmail.com>
2024-04-25 14:46:43 +0200

    Add count to store

--- frontend/lib/stores/libraries.ts
+++ frontend/lib/stores/libraries.ts
@@ -3,9 +3,13 @@ import { create } from "zustand";
 
 interface LibraryState {
     libraries: Library[]
+    count: number
+    setCount: (count: number) => void
     setLibraries(libraries: Library[]): void
 }
 export const useLibraryStore = create<LibraryState>((set) => ({
     libraries: [],
+    count: 0,
+    setCount: (count) => set({count}),
     setLibraries: (libraries) => set({libraries})
 }))
