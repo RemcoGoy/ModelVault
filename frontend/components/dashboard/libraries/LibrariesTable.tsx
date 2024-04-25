63765ff786fcdbe2c6683690b3face4ddab81629
RemcoGoy <remco.goy@hotmail.com>
2024-04-25 14:45:22 +0200

    Add actions again

--- frontend/components/dashboard/libraries/LibrariesTable.tsx
+++ frontend/components/dashboard/libraries/LibrariesTable.tsx
@@ -51,6 +51,25 @@ export default function LibrariesTable({ libraries }: { libraries: Library[] }) 
                             <TableCell>
                                 {library.created_at.toLocaleString("en-BE")}
                             </TableCell>
+                            <TableCell>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button
+                                            aria-haspopup="true"
+                                            size="icon"
+                                            variant="ghost"
+                                        >
+                                            <MoreHorizontal className="h-4 w-4" />
+                                            <span className="sr-only">Toggle menu</span>
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent align="end">
+                                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </TableCell>
                         </TableRow>
                     )
                 })}
